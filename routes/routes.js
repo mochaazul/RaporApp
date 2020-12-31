f20b6447ad01e552c1b07ab2b232e6bcf7366eb5
Mochamad Zulfikar <mochaa.zulfikar@gmail.com>
2021-01-01 00:01:26 +0700

    add walikelas routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const importRoutes = require('./importRoutes');
 const tahunPelajaranRoutes = require('./tahunPelajaranRoutes');
+const WalikelasRoutes = require('./WalikelasRoutes');
 const Routes = express.Router()
 
 Routes.use('/import',importRoutes)
 Routes.use('/tapel',tahunPelajaranRoutes)
-
+Routes.use('/walikelas',WalikelasRoutes)
 module.exports = Routes
