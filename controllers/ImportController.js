b63d3611d020d5206e3c4a84f15cc1aa97709948
Mochamad Zulfikar <mochaa.zulfikar@gmail.com>
2021-01-01 14:46:04 +0700

    add error handler to importController

--- controllers/ImportController.js
+++ controllers/ImportController.js
@@ -3,56 +3,61 @@ const XLSX = require('xlsx');
 const db = require('../config/db');
 
 class ImportController {
-  static async importSiswa(req, res) {
-    // unused logic , since we are using noSQL we do not need it
-    // let query = `CREATE TABLE siswa ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,`
+  static async importSiswa(req, res, next) {
+    try {
+      // unused logic , since we are using noSQL we do not need it
+      // let query = `CREATE TABLE siswa ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,`
 
-    // if (dataSiswa.length > 0) {
-    //   db.schema.dropTableIfExists('siswa')
-    //   dataSiswa.forEach((siswa, index) => {
+      // if (dataSiswa.length > 0) {
+      //   db.schema.dropTableIfExists('siswa')
+      //   dataSiswa.forEach((siswa, index) => {
 
-    //     if (index === 0) {          
-    //       // Ambil key dari data siswa pada index ke 0
-    //       // Hapus object dengan key __EMPTY <- key ini di generate sama modul XLSX
-    //       let keys = Object.keys(siswa).filter(item => item !==  '__EMPTY')
+      //     if (index === 0) {          
+      //       // Ambil key dari data siswa pada index ke 0
+      //       // Hapus object dengan key __EMPTY <- key ini di generate sama modul XLSX
+      //       let keys = Object.keys(siswa).filter(item => item !==  '__EMPTY')
 
-    //       // Loop dari array berisi key lalu buat append command ke variabel query
-    //       keys.forEach(item=>{
-    //         // item.replace -> menghilangkan whitespace
-    //         // getType() return tipe data dari row pertama
-    //         query += `${item.replace(/\s+/, "")} ${getType(siswa[item])}, `
-    //       })
+      //       // Loop dari array berisi key lalu buat append command ke variabel query
+      //       keys.forEach(item=>{
+      //         // item.replace -> menghilangkan whitespace
+      //         // getType() return tipe data dari row pertama
+      //         query += `${item.replace(/\s+/, "")} ${getType(siswa[item])}, `
+      //       })
 
-    //       query += " created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)"
-    //     }
-    //   })
-    // }
+      //       query += " created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)"
+      //     }
+      //   })
+      // }
 
-    let siswa = db.collection('siswa')
-    let sheet = XLSX.readFile(req.file.path)
-    let SheetNames = sheet.SheetNames[0]
-    let dataSiswa = XLSX.utils.sheet_to_json(sheet.Sheets[SheetNames], {raw:true })
+      let siswa = db.collection('siswa')
+      let sheet = XLSX.readFile(req.file.path)
+      let SheetNames = sheet.SheetNames[0]
+      let dataSiswa = XLSX.utils.sheet_to_json(sheet.Sheets[SheetNames], { raw: true })
 
-    siswa.deleteMany({})
+      siswa.deleteMany({})
 
-    dataSiswa = dataSiswa.map(item => {
-      let key, keys = Object.keys(item);
-      let n = keys.length
-      let newobj = {}
-      let counter = 0
-      while (counter < n) {
-        if(keys[counter] !== "__EMPTY"){
-          key = keys[counter]
-          newobj[key.toLowerCase()] = item[key]
+      dataSiswa = dataSiswa.map(item => {
+        let key, keys = Object.keys(item);
+        let n = keys.length
+        let newobj = {}
+        let counter = 0
+        while (counter < n) {
+          if (keys[counter] !== "__EMPTY") {
+            key = keys[counter]
+            newobj[key.toLowerCase()] = item[key]
+          }
+          counter++
         }
-        counter ++
-      }
-      return newobj
-    })
+        return newobj
+      })
 
-    const result = await siswa.insertMany(dataSiswa, { ordered: true })
+      const result = await siswa.insertMany(dataSiswa, { ordered: true })
 
-    res.status(200).json(result)
+      res.status(200).json(result)
+
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
