a83af90d463bc79f444d6d3c9f880e4e5a1df7f9
Mochamad Zulfikar <mochaa.zulfikar@gmail.com>
2021-01-01 14:47:10 +0700

    add unhandled error status code to 500 by default

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,10 @@
 function errorHandler(err, req, res, next) {
   console.log(err)
-  res.status(err.status).json({msg:err.msg})
+  if(err.status && err.msg){
+    res.status(err.status).json({msg:err.msg})
+  }else{
+    res.status(500).json(err)
+  }
 }
 
 module.exports = errorHandler
